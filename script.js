c47c263242bbfca98db7545ae88bb6d7af624417
Subhankar Ghosh <goutamg245@gmail.com>
2024-08-17 01:15:07 +0530

    new

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "flip",
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
